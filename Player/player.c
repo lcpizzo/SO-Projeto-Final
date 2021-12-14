9cea52b6c5c9376d3d166cbe2008682532238fe2
Leonardo Chagas Pizzo <leonardo_pizzo@usp.br>
2021-12-14 18:33:27 -0300

    Add water effect

--- Player/player.c
+++ Player/player.c
@@ -55,12 +55,15 @@ int main(void)
 
     InitWindow(screenWidth, screenHeight, "Projeto SO Final - teste player && scenario");
     
-    Texture2D background = LoadTexture("../Designs/Scenario/scenario_prototype_3.png");
+    Texture2D background = LoadTexture("../Designs/Scenario/scenario_without_water_effect.png");
+    Texture2D water = LoadTexture("../Designs/Scenario/water_effect.png");
+    float water_scrolling = 0.0f;
     
-    Texture2D frogg_end = LoadTexture("../Designs/Player/frog_jumping_prototype.png");
+    
+    Texture2D frogg_end = LoadTexture("../Designs/Player/frog_jumping.png");
     
     Player frogg;
-    frogg.texture = LoadTexture("../Designs/Player/frog_prototype.png");
+    frogg.texture = LoadTexture("../Designs/Player/frog.png");
     frogg.movement.x = (float)gridSize_x;
     frogg.movement.y = (float)gridSize_y;
     frogg.position.x = (float)gridSize_x*5 + gridSize_x/2;
@@ -80,6 +83,10 @@ int main(void)
         // Update
         //----------------------------------------------------------------------------------
         updatePlayer(&frogg);
+        
+        water_scrolling -= 2.0f;
+        
+        if(water_scrolling <= -water.width) water_scrolling = 0.0f;
         //----------------------------------------------------------------------------------
 
         // Draw
@@ -89,6 +96,13 @@ int main(void)
             ClearBackground(RAYWHITE);
             
             DrawTextureEx(background, (Vector2) {0, 0}, 0.0f, 0.55f, WHITE);
+            
+            DrawTexturePro(water, 
+                                (Rectangle) {0.0f, 0.0f, water.width*2, water.height},
+                                (Rectangle) {water_scrolling, screenHeight-water.height/4, water.width*2.0f, water.height*0.5f},
+                                (Vector2) {screenWidth/2, screenHeight/2},
+                                0.0f, WHITE
+                                );
             
             for (int i = 0; i <= screenWidth/gridSize_x; i++) DrawRectangle(gridSize_x*i, 0, 1, screenHeight, SKYBLUE);
             for (int i = 0; i <= screenHeight/gridSize_y; i++) DrawRectangle(0, gridSize_y*i, screenWidth, 1, SKYBLUE);
