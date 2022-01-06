e9bdcf70f32c8311da624333abf51d6da96cbb13
guijun13 <guilhermejun13.grillo@gmail.com>
2022-01-06 19:20:29 -0300

    feat: change texts

--- src/main.cpp
+++ src/main.cpp
@@ -1,160 +1,160 @@
 #include <math.h>
 #include <semaphore.h>
-#include <unistd.h>
 #include <stdlib.h>
+#include <unistd.h>
 
 #include <thread>
 #include <vector>
 
 #include "../useHeaders/horizontalScroller.h"
+#include "../useHeaders/manager.h"
 #include "../useHeaders/player.h"
 #include "../useHeaders/utils.h"
-#include "../useHeaders/manager.h"
 #include "raylib.h"
 
 typedef enum GameScreen {
-    TITLE = 0,
-    GAMEPLAY = 1
+  TITLE = 0,
+  GAMEPLAY = 1
 } GameScreen;
 
 int main(void) {
-    // Initialization
-    //--------------------------------------------------------------------------------------
-    const int GRID_CONST_X = 10;
-    const int GRID_CONST_Y = 7;
-
-    const int screenWidth = GRID_CONST_X * 85;
-    const int screenHeight = GRID_CONST_Y * 85;
-
-    const int gridSize_x = screenWidth / GRID_CONST_X;
-    const int gridSize_y = screenHeight / GRID_CONST_Y;
-
-    InitWindow(screenWidth, screenHeight, "Projeto SO Final");
-
-    GameScreen currentScreen = TITLE;
-
-    Texture2D background = LoadTexture("./Designs/Scenario/new_scenario.png");
-
-    Texture2D frogg_end = LoadTexture("./Designs/Player/frog_jumping.png");
-
-    Player frogg;
-    frogg.texture = LoadTexture("./Designs/Player/frog.png");
-    frogg.movement.x = (float)gridSize_x;
-    frogg.movement.y = (float)gridSize_y;
-    frogg.position.x = (float)gridSize_x * 5 + gridSize_x / 2;
-    frogg.position.y = (float)gridSize_y * 6 + gridSize_y / 2;
-    frogg.rotation = UP;
-    frogg.size.x = frogg.texture.width * 0.7f;
-    frogg.size.y = frogg.texture.height * 0.7f;
-
-    HorizontalScroller truck;
-    truck.texture = LoadTexture("./Designs/Enimies/truck.png");
-    truck.movement.x = (float)gridSize_x;
-    truck.position.x = (float)gridSize_x * 10;
-    truck.position.y = (float)gridSize_y * 4;
-    truck.speed.x = 0.01;
-    truck.size.x = truck.texture.width * 0.7f;
-    truck.size.y = truck.texture.height * 0.7f;
-
-    HorizontalScroller redCar;
-    redCar.texture = LoadTexture("./Designs/Enimies/red_car.png");
-    redCar.movement.x = (float)gridSize_x;
-    redCar.position.x = (float)gridSize_x * 10;
-    redCar.position.y = (float)gridSize_y * 1;
-    redCar.speed.x = 0.03;
-    redCar.size.x = redCar.texture.width * 0.7f;
-    redCar.size.y = redCar.texture.height * 0.7f;
-
-    HorizontalScroller blueCar;
-    blueCar.texture = LoadTexture("./Designs/Enimies/blue_car.png");
-    blueCar.movement.x = (float)gridSize_x;
-    blueCar.position.x = (float)gridSize_x * 10;
-    blueCar.position.y = (float)gridSize_y * 2;
-    blueCar.speed.x = 0.037;
-    blueCar.size.x = blueCar.texture.width * 0.7f;
-    blueCar.size.y = blueCar.texture.height * 0.7f;
-
-    HorizontalScroller yellowCar;
-    yellowCar.texture = LoadTexture("./Designs/Enimies/yellow_car.png");
-    yellowCar.movement.x = (float)gridSize_x;
-    yellowCar.position.x = (float)gridSize_x * 10;
-    yellowCar.position.y = (float)gridSize_y * 5;
-    yellowCar.speed.x = 0.047;
-    yellowCar.size.x = yellowCar.texture.width * 0.7f;
-    yellowCar.size.y = yellowCar.texture.height * 0.7f;
-
-    HorizontalScroller yellowCar2;
-    yellowCar2.texture = LoadTexture("./Designs/Enimies/yellow_car.png");
-    yellowCar2.movement.x = (float)gridSize_x;
-    yellowCar2.position.x = (float)gridSize_x * 10;
-    yellowCar2.position.y = (float)gridSize_y * 3;
-    yellowCar2.speed.x = 0.057;
-    yellowCar2.size.x = yellowCar2.texture.width * 0.7f;
-    yellowCar2.size.y = yellowCar2.texture.height * 0.7f;
-
-    std::vector<HorizontalScroller> enemies;
-    enemies.push_back(truck);
-    enemies.push_back(redCar);
-    enemies.push_back(blueCar);
-    enemies.push_back(yellowCar);
-    enemies.push_back(yellowCar2);
-
-    int score = 0;
-    double prev_score = score;
-    bool top = true;
-    bool end = false;
-
-    sem_t sem;
-    sem_init(&sem, 0, 1);
-
-    SetTargetFPS(60);  // Set our game to run at 60 frames-per-second
+  // Initialization
   //--------------------------------------------------------------------------------------
+  const int GRID_CONST_X = 10;
+  const int GRID_CONST_Y = 7;
 
-    // Main game loop
-    while (!WindowShouldClose())  // Detect window close button or ESC key
-    {
-        BeginDrawing();
+  const int screenWidth = GRID_CONST_X * 85;
+  const int screenHeight = GRID_CONST_Y * 85;
 
-        switch (currentScreen) {
-        case TITLE: {
-            DrawRectangle(0, 0, screenWidth, screenHeight, GREEN);
-            DrawText("SOgger", 120, 20, 40, DARKGREEN);
-            DrawText("SO + Frogger game", 120, 70, 30, DARKGREEN);
-            DrawText("Use as setinhas [<-][^][v][->] para controlar o sapo", 120, 220, 20, BLACK);
-            DrawText("Desvie dos veículos e tente chegar até o final da avenida.\nChegando lá, retorne para o início e ganhe pontos!",
-                120, 250, 20, BLACK);
-            DrawText("Pressione [ENTER] para jogar!", 120, 420, 30, BLACK);
-            if (IsKeyPressed(KEY_ENTER))
-                currentScreen = GAMEPLAY;
-        } break;
+  const int gridSize_x = screenWidth / GRID_CONST_X;
+  const int gridSize_y = screenHeight / GRID_CONST_Y;
 
-        case GAMEPLAY: {
-            prev_score = score;
-            std::thread threadCont(cont, &sem, &end, &top, &score, &enemies, &frogg, &background);
-            std::thread threadGameOver(gameOver, &sem, &end, &top, &score, &enemies, &frogg, &frogg_end);
+  InitWindow(screenWidth, screenHeight, "SOgger");
 
-            threadCont.join();
-            threadGameOver.join();
+  GameScreen currentScreen = TITLE;
 
-            if (score != prev_score) {
-                enemies[0].speed.x = 0.01 + score * (rand() % 30) / 100;
-                enemies[1].speed.x = 0.03 + score * (rand() % 30) / 100;
-                enemies[2].speed.x = 0.037 + score * (rand() % 30) / 100;
-                enemies[3].speed.x = 0.047 + score * (rand() % 30) / 100;
-                enemies[4].speed.x = 0.057 + score * (rand() % 30) / 100;
-            }
-        } break;
+  Texture2D background = LoadTexture("./Designs/Scenario/new_scenario.png");
 
-        default:
-            break;
+  Texture2D frogg_end = LoadTexture("./Designs/Player/frog_jumping.png");
+
+  Player frogg;
+  frogg.texture = LoadTexture("./Designs/Player/frog.png");
+  frogg.movement.x = (float)gridSize_x;
+  frogg.movement.y = (float)gridSize_y;
+  frogg.position.x = (float)gridSize_x * 5 + gridSize_x / 2;
+  frogg.position.y = (float)gridSize_y * 6 + gridSize_y / 2;
+  frogg.rotation = UP;
+  frogg.size.x = frogg.texture.width * 0.7f;
+  frogg.size.y = frogg.texture.height * 0.7f;
+
+  HorizontalScroller truck;
+  truck.texture = LoadTexture("./Designs/Enimies/truck.png");
+  truck.movement.x = (float)gridSize_x;
+  truck.position.x = (float)gridSize_x * 10;
+  truck.position.y = (float)gridSize_y * 4;
+  truck.speed.x = 0.025;
+  truck.size.x = truck.texture.width * 0.7f;
+  truck.size.y = truck.texture.height * 0.7f;
+
+  HorizontalScroller redCar;
+  redCar.texture = LoadTexture("./Designs/Enimies/red_car.png");
+  redCar.movement.x = (float)gridSize_x;
+  redCar.position.x = (float)gridSize_x * 10;
+  redCar.position.y = (float)gridSize_y * 1;
+  redCar.speed.x = 0.03;
+  redCar.size.x = redCar.texture.width * 0.7f;
+  redCar.size.y = redCar.texture.height * 0.7f;
+
+  HorizontalScroller blueCar;
+  blueCar.texture = LoadTexture("./Designs/Enimies/blue_car.png");
+  blueCar.movement.x = (float)gridSize_x;
+  blueCar.position.x = (float)gridSize_x * 10;
+  blueCar.position.y = (float)gridSize_y * 2;
+  blueCar.speed.x = 0.037;
+  blueCar.size.x = blueCar.texture.width * 0.7f;
+  blueCar.size.y = blueCar.texture.height * 0.7f;
+
+  HorizontalScroller yellowCar;
+  yellowCar.texture = LoadTexture("./Designs/Enimies/yellow_car.png");
+  yellowCar.movement.x = (float)gridSize_x;
+  yellowCar.position.x = (float)gridSize_x * 10;
+  yellowCar.position.y = (float)gridSize_y * 5;
+  yellowCar.speed.x = 0.047;
+  yellowCar.size.x = yellowCar.texture.width * 0.7f;
+  yellowCar.size.y = yellowCar.texture.height * 0.7f;
+
+  HorizontalScroller yellowCar2;
+  yellowCar2.texture = LoadTexture("./Designs/Enimies/yellow_car.png");
+  yellowCar2.movement.x = (float)gridSize_x;
+  yellowCar2.position.x = (float)gridSize_x * 10;
+  yellowCar2.position.y = (float)gridSize_y * 3;
+  yellowCar2.speed.x = 0.057;
+  yellowCar2.size.x = yellowCar2.texture.width * 0.7f;
+  yellowCar2.size.y = yellowCar2.texture.height * 0.7f;
+
+  std::vector<HorizontalScroller> enemies;
+  enemies.push_back(truck);
+  enemies.push_back(redCar);
+  enemies.push_back(blueCar);
+  enemies.push_back(yellowCar);
+  enemies.push_back(yellowCar2);
+
+  int score = 0;
+  double prev_score = score;
+  bool top = true;
+  bool end = false;
+
+  sem_t sem;
+  sem_init(&sem, 0, 1);
+
+  SetTargetFPS(60);  // Set our game to run at 60 frames-per-second
+                     //--------------------------------------------------------------------------------------
+
+  // Main game loop
+  while (!WindowShouldClose())  // Detect window close button or ESC key
+  {
+    BeginDrawing();
+
+    switch (currentScreen) {
+      case TITLE: {
+        DrawRectangle(0, 0, screenWidth, screenHeight, GREEN);
+        DrawText("SOgger", 120, 20, 40, DARKGREEN);
+        DrawText("SO + Frogger game", 120, 70, 30, DARKGREEN);
+        DrawText("Use as setinhas [<-][^][v][->] para controlar o sapo", 120, 220, 20, BLACK);
+        DrawText("Desvie dos veículos e tente chegar até o final da avenida.\nChegando lá, retorne para o início e ganhe pontos!",
+                 120, 250, 20, BLACK);
+        DrawText("Pressione [ENTER] para jogar!", 120, 420, 30, BLACK);
+        if (IsKeyPressed(KEY_ENTER))
+          currentScreen = GAMEPLAY;
+      } break;
+
+      case GAMEPLAY: {
+        prev_score = score;
+        std::thread threadCont(cont, &sem, &end, &top, &score, &enemies, &frogg, &background);
+        std::thread threadGameOver(gameOver, &sem, &end, &top, &score, &enemies, &frogg, &frogg_end);
+
+        threadCont.join();
+        threadGameOver.join();
+
+        if (score != prev_score) {
+          enemies[0].speed.x = 0.01 + score * (rand() % 30) / 100;
+          enemies[1].speed.x = 0.03 + score * (rand() % 30) / 100;
+          enemies[2].speed.x = 0.037 + score * (rand() % 30) / 100;
+          enemies[3].speed.x = 0.047 + score * (rand() % 30) / 100;
+          enemies[4].speed.x = 0.057 + score * (rand() % 30) / 100;
         }
-        EndDrawing();
-    }
+      } break;
 
-    // De-Initialization
-    //--------------------------------------------------------------------------------------
-    CloseWindow();  // Close window and OpenGL context
-    sem_destroy(&sem);
-    //--------------------------------------------------------------------------------------
-    return 0;
+      default:
+        break;
+    }
+    EndDrawing();
+  }
+
+  // De-Initialization
+  //--------------------------------------------------------------------------------------
+  CloseWindow();  // Close window and OpenGL context
+  sem_destroy(&sem);
+  //--------------------------------------------------------------------------------------
+  return 0;
 }
--- src/manager.cpp
+++ src/manager.cpp
@@ -1,90 +1,86 @@
 #include "../useHeaders/manager.h"
 
-#include "../useHeaders/horizontalScroller.h"
-#include "../useHeaders/player.h"
-#include "../useHeaders/utils.h"
-
 #include <semaphore.h>
 #include <unistd.h>
-#include <vector>
+
 #include <iostream>
 #include <thread>
 #include <typeinfo>
+#include <vector>
 
-
+#include "../useHeaders/horizontalScroller.h"
+#include "../useHeaders/player.h"
+#include "../useHeaders/utils.h"
 #include "raylib.h"
 
 void cont(sem_t *sem, bool *end, bool *top, int *score, std::vector<HorizontalScroller> *enemies, Player *frogg, Texture2D *background) {
-    if(!*end){
-        updatePlayer(frogg);
-        updateEnemies(enemies);
-        checkCollision(sem, *end, frogg->position.x, frogg->position.y, enemies);
+  if (!*end) {
+    updatePlayer(frogg);
+    updateEnemies(enemies);
+    checkCollision(sem, *end, frogg->position.x, frogg->position.y, enemies);
 
-        if(*end){
-            sem_post(sem);
-            sem_wait(sem);
-        }
-
-        updateScore(frogg, *score, *top);
-
-        ClearBackground(RAYWHITE);
-
-        DrawTextureEx(*background, (Vector2){0, 0}, 0.0f, 0.55f, WHITE);
-
-        if(*end){
-            DrawRectangle(0, GetScreenHeight() / 2 - 70, GetScreenWidth(), 70, WHITE);
-            DrawText("GAME OVER", GetScreenWidth() / 2 - MeasureText("GAME OVER", 35) / 2, GetScreenHeight() / 2 - 50, 35, DARKGRAY);
-        }
-
-        DrawTexturePro(frogg->texture,                                                                // texture
-                    (Rectangle){0.0f, 0.0f, frogg->texture.width, frogg->texture.height},           // Source Rectangle = parte da textura que será usada
-                    (Rectangle){frogg->position.x, frogg->position.y, frogg->size.x, frogg->size.y},  // Destination Rectangle = posição na tela
-                    (Vector2){frogg->size.x / 2, frogg->size.y / 2},                                // Origin = ponto de rotação da textura
-                    frogg->rotation * 90.0f, WHITE);                                               // Rotation, colour
-
-        for (int i = 0; i <= enemies->size(); i++) {
-            DrawTexturePro((*enemies)[i].texture,
-                    (Rectangle){0.0f, 0.0f, (*enemies)[i].texture.width, (*enemies)[i].texture.height},
-                    (Rectangle){(*enemies)[i].position.x, (*enemies)[i].position.y, (*enemies)[i].size.x, (*enemies)[i].size.y},
-                    (Vector2){4, 4},
-                    UP, WHITE);
-        }
-        
-        DrawText("move the frogg with arrow keys", 10, 10, 20, DARKGRAY);
-        DrawText(TextFormat("Score: %d", *score * 100), GetScreenWidth() / 2 - 100, 50, 50, WHITE);
+    if (*end) {
+      sem_post(sem);
+      sem_wait(sem);
     }
+
+    updateScore(frogg, *score, *top);
+
+    ClearBackground(RAYWHITE);
+
+    DrawTextureEx(*background, (Vector2){0, 0}, 0.0f, 0.55f, WHITE);
+
+    if (*end) {
+      DrawRectangle(0, GetScreenHeight() / 2 - 70, GetScreenWidth(), 70, WHITE);
+      DrawText("GAME OVER", GetScreenWidth() / 2 - MeasureText("GAME OVER", 35) / 2, GetScreenHeight() / 2 - 50, 35, DARKGRAY);
+    }
+
+    DrawTexturePro(frogg->texture,                                                                   // texture
+                   (Rectangle){0.0f, 0.0f, frogg->texture.width, frogg->texture.height},             // Source Rectangle = parte da textura que serï¿½ usada
+                   (Rectangle){frogg->position.x, frogg->position.y, frogg->size.x, frogg->size.y},  // Destination Rectangle = posiï¿½ï¿½o na tela
+                   (Vector2){frogg->size.x / 2, frogg->size.y / 2},                                  // Origin = ponto de rotaï¿½ï¿½o da textura
+                   frogg->rotation * 90.0f, WHITE);                                                  // Rotation, colour
+
+    for (int i = 0; i <= enemies->size(); i++) {
+      DrawTexturePro((*enemies)[i].texture,
+                     (Rectangle){0.0f, 0.0f, (*enemies)[i].texture.width, (*enemies)[i].texture.height},
+                     (Rectangle){(*enemies)[i].position.x, (*enemies)[i].position.y, (*enemies)[i].size.x, (*enemies)[i].size.y},
+                     (Vector2){4, 4},
+                     UP, WHITE);
+    }
+
+    DrawText(TextFormat("Score: %d", *score * 100), 10, 10, 50, WHITE);
+  }
 }
 
 void gameOver(sem_t *sem, bool *end, bool *top, int *score, std::vector<HorizontalScroller> *enemies, Player *frogg, Texture2D *frogg_end) {
-    if(*end){
-        sem_wait(sem);
-        DrawRectangle(0, GetScreenHeight() / 2 - 70, GetScreenWidth(), 70, WHITE);
-        DrawText("GAME OVER", GetScreenWidth() / 2 - MeasureText("GAME OVER", 35) / 2, GetScreenHeight() / 2 - 50, 35, DARKGRAY);
-        sleep(2);
+  if (*end) {
+    sem_wait(sem);
+    DrawRectangle(0, GetScreenHeight() / 2 - 70, GetScreenWidth(), 70, WHITE);
+    DrawText("GAME OVER", GetScreenWidth() / 2 - MeasureText("GAME OVER", 35) / 2, GetScreenHeight() / 2 - 50, 35, DARKGRAY);
+    sleep(2);
 
-        *top = true;
-        *score = 0;
-        *end = false;
-        
+    *top = true;
+    *score = 0;
+    *end = false;
 
-        frogg->position.x = (float)frogg->movement.x * 5 + frogg->movement.x / 2;
-        frogg->position.y = (float)frogg->movement.y * 6 + frogg->movement.y / 2;
-        frogg->rotation = UP;
+    frogg->position.x = (float)frogg->movement.x * 5 + frogg->movement.x / 2;
+    frogg->position.y = (float)frogg->movement.y * 6 + frogg->movement.y / 2;
+    frogg->rotation = UP;
 
-        for (int i = 0; i <= enemies->size(); i++) {
-            DrawTexturePro((*enemies)[i].texture,
-                           (Rectangle){0.0f, 0.0f, (*enemies)[i].texture.width, (*enemies)[i].texture.height},
-                           (Rectangle){(*enemies)[i].position.x, (*enemies)[i].position.y, (*enemies)[i].size.x, (*enemies)[i].size.y},
-                           (Vector2){4, 4},
-                           UP, WHITE);
-        }
-        DrawTexturePro(*frogg_end,
-                           (Rectangle){0.0f, 0.0f, frogg_end->width, frogg_end->height},
-                           (Rectangle){frogg->position.x, frogg->position.y, frogg_end->width * 0.6, frogg_end->height * 0.6},
-                           (Vector2){frogg->size.x / 2, frogg->size.y / 2},
-                           frogg->rotation * 90.0f, WHITE);
-
-        sem_post(sem);
+    for (int i = 0; i <= enemies->size(); i++) {
+      DrawTexturePro((*enemies)[i].texture,
+                     (Rectangle){0.0f, 0.0f, (*enemies)[i].texture.width, (*enemies)[i].texture.height},
+                     (Rectangle){(*enemies)[i].position.x, (*enemies)[i].position.y, (*enemies)[i].size.x, (*enemies)[i].size.y},
+                     (Vector2){4, 4},
+                     UP, WHITE);
     }
-}
+    DrawTexturePro(*frogg_end,
+                   (Rectangle){0.0f, 0.0f, frogg_end->width, frogg_end->height},
+                   (Rectangle){frogg->position.x, frogg->position.y, frogg_end->width * 0.6, frogg_end->height * 0.6},
+                   (Vector2){frogg->size.x / 2, frogg->size.y / 2},
+                   frogg->rotation * 90.0f, WHITE);
 
+    sem_post(sem);
+  }
+}
